dc052347a59c968fb212c5d6ff223e914c013e29
jyanev01 <jordanyanev@gmail.com>
2021-10-30 15:37:41 -0400

    badges updated"

--- index.js
+++ index.js
@@ -2,97 +2,138 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
-let testObj = {
-    title: "Harry Potter",
-    description: "You're a wizard!"
-}
+// let testObj = {
+//     title: "Harry Potter",
+//     description: "You're a wizard!"
+// }
 
 // console.log(generateMarkdown(testObj))
 // fs.writeFileSync("test.md", generateMarkdown(testObj))
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Please provide the name of your project!',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please provide a project title!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please provide a project title!');
-                return false;
-            }
-        }
-    },
+    // {
+    //     type: 'input',
+    //     name: 'title',
+    //     message: 'What is the name of your project!',
+    //     validate: titleInput => {
+    //         if (titleInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please provide a project title!');
+    //             return false;
+    //         }
+    //     }
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'name',
+    //     message: 'What is your name?',
+    //     validate: nameInput => {
+    //         if (nameInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please provide your name!');
+    //             return false;
+    //         }
+    //     }
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'email',
+    //     message: 'What is your email address?',
+    //     validate: emailInput => {
+    //         if (emailInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please enter an email address!');
+    //             return false;
+    //         }
+    //     }
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'github',
+    //     message: 'What is your GitHub account?',
+    //     validate: githubInput => {
+    //         if(githubInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please provide your GitHub account!');
+    //             return false;
+    //         }
+    //     }
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'description',
+    //     message: "Please provide a brief description of your project!",
+    //     validate: descriptionInput => {
+    //         if (descriptionInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please provide a project description!');
+    //             return false;
+    //         }
+    //     }
+    // },
     {
         type: 'list',
         name: 'license',
-        message: 'yo, license pls:',
-        choices: ['MIT', 'ISC', 'Eclipse', 'Mozilla', 'WTFPL']
+        message: 'What kind of license does your project use? Please select One:',
+        choices: ['MIT', 'ISC', 'Eclipse', 'Mozilla', 'WTFPL', 'Unknown'],
+        default: 'MIT',
     },
-    
     // {
     //     type: 'input',
-    //     name: 'title',
-    //     message: 'Please provide the name of your project!',
-    //     validate: nameInput => {
-    //         if (nameInput) {
+    //     name: 'usage',
+    //     message: 'Please describe how to use your project?',
+    //     validate: usageInput => {
+    //         if (usageInput) {
     //             return true;
     //         } else {
-    //             console.log('Please provide a project title!');
+    //             console.log('Please brief details on usage/ applications!');
     //             return false;
     //         }
     //     }
     // },
     // {
     //     type: 'input',
-    //     name: 'title',
-    //     message: 'Please provide the name of your project!',
-    //     validate: nameInput => {
-    //         if (nameInput) {
-    //             return true;
-    //         } else {
-    //             console.log('Please provide a project title!');
-    //             return false;
-    //         }
-    //     }
+    //     name: 'contributors',
+    //     message: "How can a user contribute to the this repo?",
     // },
-
+    // {
+    //     type: 'input',
+    //     name: 'test',
+    //     message: 'How can a test be run on your project?',
+    //     default: 'npm test'
+    // }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(`./dist/${fileName}`, data, err => {
+function writeToFile(data) {
+    fs.writeFile('ReadME.md', data, err => {
         if (err) {
             throw err
-        };
-        console.log('README created')
-    });
-
+        } else {
+            console.log('README created!');
+        }
+    })
 };
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(answers => {
-        fs.writeFile("../Professional_README_generator/dist/test.md", generateMarkdown(answers), err => {
-            if (err) throw err;
+        .then(answers => {
+            return generateMarkdown(answers);
         })
-    })
-};
+        .then(data => {
+            return writeToFile(data);
+        })
+    err => {
+        if (err) throw err;
+    }
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-
   if(license == 'MIT'){
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
   } else if (license == 'ISC') {
@@ -23,51 +12,75 @@ function renderLicenseSection(license) {
   } else if (license == 'WTFPL') {
     return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
   } else {
-    return;
+    return `No License`;
   }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+//included at the end of the renderLicenseSection() as part of the last 'else' statement 
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(license == 'MIT'){
+    return `MIT `;
+  } else if (license == 'ISC') {
+    return `ISC `;
+  } else if (license == 'Eclipse') {
+    return `Eclipse `;
+  } else if (license == 'Mozilla') {
+    return ` MPL 2.0 `;
+  } else if (license == 'WTFPL') {
+    return `WTFPL `;
+  } else {
+    return `No license selected!`;
+  }
+
+  
 };
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const license = renderLicenseSection(data.license)
+  const license = renderLicenseSection (data.license) +  renderLicenseBadge(data.license)
 
   return `# ${data.title}
-
-  ## Table of Contents
-  -Description
-  -Installation
-  -Usage
-  -License
-  -Contributions
-  -Tests
-  -Contact
+  # ${license}
 
   ## Description
   ${data.description}
 
+  ## Table of Contents
+  * [Installation] (#installation)
+  * [Usage] (#usage)
+  * [License] (#license)
+  * [Contributions] (#contributions)
+  * [Tests] (#test)
+  * [Contact] (#contact)
+
+  
+
   ## Installation
+  * INSTALL: npm install
+  * RUN: node index.js
 
   ## Usage
   ${data.usage}
 
-  ##License
+  ## License
   ${license}
 
-  ##Contributions
+  ## Contributions
   ${data.contributions}
 
-  ##Tests
+  ## Tests
   ${data.tests}
 
-  ##Contact
-  ${data.contact}
-
-
-Write markdown syntax
-and the object that you input
-through the function
-${data.name}
-${license}
+  ## Contact
+  If you have any questions please contact me ${data.name} at: ${data.email} 
+  For more projects please visit my GitHub : https://github.com/${data.github}
 `;
 }
 
