49aabb906cbe0314b831e9cf3be75d16639f3ed6
jyanev01 <jordanyanev@gmail.com>
2021-10-30 10:57:15 -0400

    license badges added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+let testObj = {
+    title: "Harry Potter",
+    description: "You're a wizard!"
+}
+
+// console.log(generateMarkdown(testObj))
+// fs.writeFileSync("test.md", generateMarkdown(testObj))
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -31,31 +38,38 @@ const questions = [
         }
     },
     {
-        type: 'input',
-        name: 'title',
-        message: 'Please provide the name of your project!',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please provide a project title!');
-                return false;
-            }
-        }
+        type: 'list',
+        name: 'license',
+        message: 'yo, license pls:',
+        choices: ['MIT', 'ISC', 'Eclipse', 'Mozilla', 'WTFPL']
     },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Please provide the name of your project!',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please provide a project title!');
-                return false;
-            }
-        }
-    },
+    
+    // {
+    //     type: 'input',
+    //     name: 'title',
+    //     message: 'Please provide the name of your project!',
+    //     validate: nameInput => {
+    //         if (nameInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please provide a project title!');
+    //             return false;
+    //         }
+    //     }
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'title',
+    //     message: 'Please provide the name of your project!',
+    //     validate: nameInput => {
+    //         if (nameInput) {
+    //             return true;
+    //         } else {
+    //             console.log('Please provide a project title!');
+    //             return false;
+    //         }
+    //     }
+    // },
 
 ];
 
@@ -72,7 +86,12 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    return inquirer.prompt(questions);
+    inquirer.prompt(questions)
+    .then(answers => {
+        fs.writeFile("../Professional_README_generator/dist/test.md", generateMarkdown(answers), err => {
+            if (err) throw err;
+        })
+    })
 };
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +10,64 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  if(license == 'MIT'){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license == 'ISC') {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+  } else if (license == 'Eclipse') {
+    return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+  } else if (license == 'Mozilla') {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license == 'WTFPL') {
+    return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
+  } else {
+    return;
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const license = renderLicenseSection(data.license)
+
   return `# ${data.title}
 
+  ## Table of Contents
+  -Description
+  -Installation
+  -Usage
+  -License
+  -Contributions
+  -Tests
+  -Contact
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+
+  ## Usage
+  ${data.usage}
+
+  ##License
+  ${license}
+
+  ##Contributions
+  ${data.contributions}
+
+  ##Tests
+  ${data.tests}
+
+  ##Contact
+  ${data.contact}
+
+
+Write markdown syntax
+and the object that you input
+through the function
+${data.name}
+${license}
 `;
 }
 
